f975eac1daab5d27633ad0f823fe31c328e7491b
knokko <knokogator@hotmail.com>
2024-11-17 11:34:04 +0100

    Fix small error in the srgbToLinear function

--- src/test/resources/com/github/knokko/compressor/kim1.glsl
+++ src/test/resources/com/github/knokko/compressor/kim1.glsl
@@ -1,6 +1,6 @@
 float srgbToLinear(float srgb) {
 	if (srgb <= 0.04) return srgb / 12.92;
-	else return pow((srgb + 0.044) / 1.055, 2.4);
+	else return pow((srgb + 0.055) / 1.055, 2.4);
 }
 
 vec3 srgbToLinear(vec3 srgb) {
